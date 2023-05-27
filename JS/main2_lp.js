bbb153048cf4c344b53b6049514033e137c82ba7
Ana Julia Moura Martins <ana.martins_2004@outlook.com>
2023-05-26 21:01:26 -0300

    Section das disciplinas

--- JS/main2_lp.js
+++ JS/main2_lp.js
@@ -1,7 +1,9 @@
-var divs_explicacao = document.getElementsByClassName("div_explicacao");
+//Lógica section 1 : Introdução
+var divs_explicacao = document.getElementsByClassName("explicacao");
 
 var id_explicacao_cW = "explicacao_comunicacaoWeb";
 var id_explicacao_c = "explicacao_conceito";
+var id_explicacao_ide = "explicacao_ide";
 
 div_comunicacaoWeb = document.getElementById("comunicacao_web");
 div_comunicacaoWeb.addEventListener("mouseover", function() {
@@ -13,19 +15,23 @@ div_conceitos.addEventListener("mouseover", function() {
     tornarDisplayFlex(id_explicacao_c);
   });
 
+div_conceitos = document.getElementById("ide");
+div_conceitos.addEventListener("mouseover", function() {
+    tornarDisplayFlex(id_explicacao_ide);
+  });
+
 
 
 function tornarDisplayNone(){
-    //Itinerar um array deixando todos ele none
+    //Deixar todos os display invisíveis
     for(let i=0; i< divs_explicacao.length; i++){
         divs_explicacao[i].style.display = "none";
-        console.log("display none")
-    }
-    
+    }   
 }
 function tornarDisplayFlex(id_elemento){
-    //Tornar todos none
+    //Tornar todos invisíveis (display= "none")
     tornarDisplayNone()
+
     //Tornar 1 display flex
     let elemento = document.getElementById(id_elemento);
     elemento.style.display = "flex"
@@ -34,47 +40,3 @@ function tornarDisplayFlex(id_elemento){
 }
 
 
-
-//Função para mudar o conteúdo da div
-
-//Tentando uma lógica nova
-
-//1° Função que deixa todos os display "none"
-//Função que deixa apenas a div do id passado flex
-
-//Array com todas as div de explicação
-
-
-/*
-function mudarConteudo(){
-    //Pegar a div de explicacao comunicação web
-    div_explicacao_comunicacaoWeb = document.getElementById("explicacao_comunicacaoWeb");
-    //Pegar a div de conceito
-    div_explicacao_conceito = document.getElementById("explicacao_conceito");
-
-    //Tornar a div de C.W em display none
-    div_explicacao_comunicacaoWeb.style.display = "flex";
-
-    //Tornar a div Conc em display flex
-    div_explicacao_conceito.style.display = "none"
-
-    console.log('oi');
-
-}
-
-function mudarConteudo_conceitos(){
-    //Pegar a div de explicacao comunicação web
-    div_explicacao_comunicacaoWeb = document.getElementById("explicacao_comunicacaoWeb");
-    //Pegar a div de conceito
-    div_explicacao_conceito = document.getElementById("explicacao_conceito");
-
-    //Tornar a div de C.W em display none
-    div_explicacao_comunicacaoWeb.style.display = "none";
-
-    //Tornar a div Conc em display flex
-    div_explicacao_conceito.style.display = "flex"
-
-}
-
-
-*/
