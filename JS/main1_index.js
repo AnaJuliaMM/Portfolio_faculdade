ebcadd478b9a86dc45e094e3cf5d2827fea10295
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-06-09 19:42:42 -0300

    Seção primeiras atividades e rebuild do header lp

--- JS/main1_index.js
+++ JS/main1_index.js
@@ -1,7 +1,10 @@
 //Mudança de Janela
-linguagemProgramacao = document.getElementById("cc_lingP");
-linguagemProgramacao.addEventListener("click", mudarJanela)
+var disciplinas = document.getElementsByClassName("cc")
+var paginas = ["linguagemProgramacao.html", "automacaoIndustrial.html"]
+disciplinas[0].addEventListener("click", function(){mudarJanela(0)} )
+disciplinas[1].addEventListener("click", function(){mudarJanela(1)} )
 
-function mudarJanela (){
-    window.location.href = "linguagemProgramacao.html"
+
+function mudarJanela (index){
+    window.location.href = paginas[index]
 }
