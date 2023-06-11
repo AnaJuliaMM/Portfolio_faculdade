02f4a84d190f3d4905c920c02a799433500ba78d
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-06-10 23:59:45 -0300

    comecando 03_redesComputadores

--- JS/main1_index.js
+++ JS/main1_index.js
@@ -1,8 +1,10 @@
 //Mudança de Janela
 var disciplinas = document.getElementsByClassName("cc")
-var paginas = ["01_linguagemProgramacao.html", "02_automacaoIndustrial.html"]
+var paginas = ["01_linguagemProgramacao.html", "02_automacaoIndustrial.html", "03_redesComputadores.html"]
 disciplinas[0].addEventListener("click", function(){mudarJanela(0)} )
 disciplinas[1].addEventListener("click", function(){mudarJanela(1)} )
+disciplinas[2].addEventListener("click", function(){mudarJanela(2)} )
+
 
 
 function mudarJanela (index){
