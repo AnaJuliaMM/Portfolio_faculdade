24f1838f6a7bb6fc6639ea3ccaacf335fc09eaf1
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-06-13 01:27:25 -0300

    estilização redes

--- JS/main4.js
+++ JS/main4.js
@@ -17,12 +17,17 @@ function tornarDisplayFlex(indice, elementos){
 
 
 
-//Variáveis Redes de computadores
-var btn_protocolo = document.getElementsByClassName("protocolos")
-var explicacao_camada_osi = document.getElementsByClassName("explicacao_camda_OSI")
+//Variáveis de Introducao Redes de computadores
+var btn_introducao = document.getElementsByClassName("item_redes")
+var explicacao_introducao = document.getElementsByClassName("body_introducao")
 
-btn_protocolo[1].addEventListener("mouseover", function (){tornarDisplayFlex(0,explicacao_camada_osi)})
-btn_protocolo[2].addEventListener("mouseover", function (){tornarDisplayFlex(1,explicacao_camada_osi)})
+btn_introducao[0].addEventListener("mouseover", function (){tornarDisplayFlex(0,explicacao_introducao)})
+btn_introducao[1].addEventListener("mouseover", function (){tornarDisplayFlex(1,explicacao_introducao)})
 
 
+//Variáveis de Transmissão
+var btn_transmissao = document.getElementsByClassName("item_transmissao")
+var explicacao_transmissao = document.getElementsByClassName("body_transmissao")
 
+btn_transmissao[0].addEventListener("mouseover", function (){tornarDisplayFlex(0,explicacao_transmissao)})
+btn_transmissao[1].addEventListener("mouseover", function (){tornarDisplayFlex(1,explicacao_transmissao)})
