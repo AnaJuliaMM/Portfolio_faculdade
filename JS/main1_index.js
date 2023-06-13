6ca8fd941fa05f6eb42fea38d2378aab7ea7da13
Ana Julia Moura Martins <ana.martins_2004@outlook.com>
2023-06-12 22:44:02 -0300

    Redes

--- JS/main1_index.js
+++ JS/main1_index.js
@@ -1,9 +1,11 @@
 //Mudança de Janela
 var disciplinas = document.getElementsByClassName("cc")
-var paginas = ["01_linguagemProgramacao.html", "02_automacaoIndustrial.html", "03_redesComputadores.html"]
+var paginas = ["01_linguagemProgramacao.html", "02_automacaoIndustrial.html", "03_redesComputadores.html", "04_bancoDados.html"]
 disciplinas[0].addEventListener("click", function(){mudarJanela(0)} )
 disciplinas[1].addEventListener("click", function(){mudarJanela(1)} )
 disciplinas[2].addEventListener("click", function(){mudarJanela(2)} )
+disciplinas[3].addEventListener("click", function(){mudarJanela(3)} )
+
 
 
 
--- JS/main4.js
+++ JS/main4.js
@@ -1,9 +1,28 @@
 //Funções
-
+function tornarDisplayNone(elementos){
+    //Deixar todos os display invisíveis
+    for(let i=0; i< elementos.length; i++){
+      elementos[i].style.display = "none";
+      console.log("display none")
+    }  
+}
+function tornarDisplayFlex(indice, elementos){
+  //Manda para o ser none todo o array
+  tornarDisplayNone(elementos)
+  
+  //Tornar display flex
+  elementos[indice].style.display = "flex"
+  console.log("display flex")
+}
 
 
 
 //Variáveis Redes de computadores
-var btn_camada_osi = document.getElementsByClassName("camadas_osi")
+var btn_protocolo = document.getElementsByClassName("protocolos")
 var explicacao_camada_osi = document.getElementsByClassName("explicacao_camda_OSI")
+
+btn_protocolo[1].addEventListener("mouseover", function (){tornarDisplayFlex(0,explicacao_camada_osi)})
+btn_protocolo[2].addEventListener("mouseover", function (){tornarDisplayFlex(1,explicacao_camada_osi)})
+
+
 
