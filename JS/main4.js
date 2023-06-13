618cb7c90bdfb761c5f16ec7829f30aa3e8c3f4b
Ana Julia Moura Martins <ana.martins_2004@outlook.com>
2023-06-12 21:02:40 -0300

    redes

---
+++ JS/main4.js
@@ -0,0 +1,9 @@
+//Funções
+
+
+
+
+//Variáveis Redes de computadores
+var btn_camada_osi = document.getElementsByClassName("camadas_osi")
+var explicacao_camada_osi = document.getElementsByClassName("explicacao_camda_OSI")
+
