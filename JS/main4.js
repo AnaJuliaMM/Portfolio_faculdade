f4b34fa8aa6b7c8c9374850309dbbe7355fcc1e6
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-06-13 10:22:28 -0300

    MPV do 03_redesComputadores

--- JS/main4.js
+++ JS/main4.js
@@ -24,6 +24,13 @@ var explicacao_introducao = document.getElementsByClassName("body_introducao")
 btn_introducao[0].addEventListener("mouseover", function (){tornarDisplayFlex(0,explicacao_introducao)})
 btn_introducao[1].addEventListener("mouseover", function (){tornarDisplayFlex(1,explicacao_introducao)})
 
+//Variáveis de equipamentos
+var btn_equipamentos= document.getElementsByClassName("item_equipamento")
+var explicacao_equipamentos = document.getElementsByClassName("descricao_equipamentos")
+
+btn_equipamentos[0].addEventListener("mouseover", function (){tornarDisplayFlex(0,explicacao_equipamentos)})
+btn_equipamentos[1].addEventListener("mouseover", function (){tornarDisplayFlex(1,explicacao_equipamentos)})
+btn_equipamentos[2].addEventListener("mouseover", function (){tornarDisplayFlex(2,explicacao_equipamentos)})
 
 //Variáveis de Transmissão
 var btn_transmissao = document.getElementsByClassName("item_transmissao")
@@ -31,3 +38,11 @@ var explicacao_transmissao = document.getElementsByClassName("body_transmissao")
 
 btn_transmissao[0].addEventListener("mouseover", function (){tornarDisplayFlex(0,explicacao_transmissao)})
 btn_transmissao[1].addEventListener("mouseover", function (){tornarDisplayFlex(1,explicacao_transmissao)})
+
+//Variáveis do IP
+var btn_ip = document.getElementsByClassName("item_ip")
+var explicacao_ip = document.getElementsByClassName("descricao_ip")
+
+btn_ip[0].addEventListener("mouseover", function (){tornarDisplayFlex(0,explicacao_ip)})
+btn_ip[1].addEventListener("mouseover", function (){tornarDisplayFlex(1,explicacao_ip)})
+btn_ip[2].addEventListener("mouseover", function (){tornarDisplayFlex(2,explicacao_ip)})
