7c9d4c446e473cd0ae1fb9021ccad6763e6d55fc
Ana Julia Moura Martins <ana.martins_2004@outlook.com>
2023-06-14 17:48:33 -0300

    bd inicio

--- JS/main1_index.js
+++ JS/main1_index.js
@@ -1,10 +1,12 @@
 //Mudança de Janela
 var disciplinas = document.getElementsByClassName("cc")
-var paginas = ["01_linguagemProgramacao.html", "02_automacaoIndustrial.html", "03_redesComputadores.html", "04_bancoDados.html"]
+var paginas = ["01_linguagemProgramacao.html", "02_automacaoIndustrial.html", "03_redesComputadores.html", "04_bancoDados.html", "05_cienciaDados.html"]
 disciplinas[0].addEventListener("click", function(){mudarJanela(0)} )
 disciplinas[1].addEventListener("click", function(){mudarJanela(1)} )
 disciplinas[2].addEventListener("click", function(){mudarJanela(2)} )
 disciplinas[3].addEventListener("click", function(){mudarJanela(3)} )
+disciplinas[4].addEventListener("click", function(){mudarJanela(4)} )
+
 
 
 
--- JS/main6.js
+++ JS/main6.js
@@ -1,1 +1,27 @@
+//Funções
+function tornarDisplayNone(elementos){
+    //Deixar todos os display invisíveis
+    for(let i=0; i< elementos.length; i++){
+      elementos[i].style.display = "none";
+      console.log("display none")
+    }  
+}
+function tornarDisplayFlex(indice, elementos){
+  //Manda para o ser none todo o array
+  tornarDisplayNone(elementos)
+  
+  //Tornar display flex
+  elementos[indice].style.display = "flex"
+  console.log("display flex")
+}
 
+//Variáveis introdução a bando de dados
+var btn_medidas = document.getElementsByClassName("topico_mt")
+var medidas_centrais = document.getElementsByClassName("explicacao")
+
+btn_medidas[0].addEventListener("mouseover", function (){tornarDisplayFlex(0,medidas_centrais)})
+btn_medidas[1].addEventListener("mouseover", function (){tornarDisplayFlex(1,medidas_centrais)})
+btn_medidas[2].addEventListener("mouseover", function (){tornarDisplayFlex(2,medidas_centrais)})
+btn_medidas[3].addEventListener("mouseover", function (){tornarDisplayFlex(3,medidas_centrais)})
+btn_medidas[4].addEventListener("mouseover", function (){tornarDisplayFlex(4,medidas_centrais)})
+btn_medidas[5].addEventListener("mouseover", function (){tornarDisplayFlex(5,medidas_centrais)})
